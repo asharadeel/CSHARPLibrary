3fd89624d99548ace7dc764cf2c8f157a1d67cf5
Ashar Adeel <asharadeel@outlook.com>
2025-11-28 04:33:20 +0000

    TESTING 2

--- Cipher/obj/Debug/net9.0/Cipher.AssemblyInfo.cs
+++ Cipher/obj/Debug/net9.0/Cipher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cipher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d47ffe09860a958a804a28f28080b62ee4c1208")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9cd05809215fd74b18b7b376f3b9ad2fdf01fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cipher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cipher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
